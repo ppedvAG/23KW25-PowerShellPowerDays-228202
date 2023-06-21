27c67c34a2a4357d3fdd404aacc5bd8c2661f259
Stefan Ober <StefanO@ppedv.de>
2023-06-21 10:11:05 +0200

    Hilfreiches:NewAdUser

---
+++ Hilfreiches/ImportAdUserFromCsv.ps1
@@ -0,0 +1,4 @@
+﻿Import-Csv -Path C:\Testfiles\MyNewAdUsers2.csv | ForEach-Object -Process {
+            New-ADUser -GivenName $PSItem.GivenName `                       -Surname $PSItem.SurName `                       -Name $PSItem.Name `                       -SamAccountName $PSItem.SamAccountName `
+                       -Department $PSItem.Department `                       -Path $PSItem.Path `                       -Enabled ([bool]::Parse($PSItem.Enabled)) `                       -AccountPassword (ConvertTo-SecureString $PSItem.Password -AsPlainText -Force)
+}
