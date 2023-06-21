024b3b065ce58516d6a4e6feb8711ea95e516f9b
Stefan Ober <StefanO@ppedv.de>
2023-06-21 14:27:02 +0200

    Skript mit optionaler Verbose Ausgabe

--- Examples/Get-EventLog.ps1
+++ Examples/Get-EventLog.ps1
@@ -1,4 +1,5 @@
-﻿Param(
+﻿[cmdletBinding()]
+param(
 
 [string]$ComputerName = "localhost",
 
@@ -7,7 +8,13 @@
 
 [int]$Newest = 5
 )
+$Startzeit = Get-Date
+Write-Verbose -Message "Vom User wurden folgende Werte angegeben:"
+Write-Verbose -Message "EventID: $EventId"
+Write-Verbose -Message "Computername: $ComputerName"
+Write-Verbose -Message "Newest: $Newest"
 
 Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventId -eq $EventId | Select-Object -First $Newest
 
-
+$Laufzeit = (Get-Date) - $Startzeit 
+Write-Verbose -Message ("Laufzeit(sec): " + $Laufzeit.TotalSeconds )
