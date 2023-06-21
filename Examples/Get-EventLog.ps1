733aed608f439be3b34bc8420c511d587fd55918
Stefan Ober <StefanO@ppedv.de>
2023-06-21 14:43:51 +0200

    Skript mit CommentBasedHelp

--- Examples/Get-EventLog.ps1
+++ Examples/Get-EventLog.ps1
@@ -1,4 +1,26 @@
-﻿[cmdletBinding()]
+﻿<#
+.SYNOPSIS
+ Kurzbeschreibung des Skriptes
+.DESCRIPTION
+ Lange ausführliche Beschreibung des Skriptzweckes
+.PARAMETER EventId
+ 4624 | Anmeldung
+ 4625 | fehlgeschlagene Anmeldung
+ 4634 | Abmeldung
+.EXAMPLE
+ Get-EventLog.ps1 -EventId 4624
+
+ Index Time          EntryType   Source                 InstanceID Message
+ ----- ----          ---------   ------                 ---------- -------
+556827 Jun 21 14:25  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+556824 Jun 21 14:25  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+556820 Jun 21 14:24  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+556815 Jun 21 14:23  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+556812 Jun 21 14:23  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+.LINK
+ https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1#syntax-for-comment-based-help
+#>
+[cmdletBinding()]
 param(
 
 [string]$ComputerName = "localhost",
